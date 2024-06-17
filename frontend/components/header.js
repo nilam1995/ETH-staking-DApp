712f31c9e399f2f1e759e45877dba6d586b3b698
nilam1995 <72725548+nilam1995@users.noreply.github.com>
2024-06-17 20:53:35 +0530

    header added

---
+++ frontend/components/header.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { Beans } from "@web3uikit/icons";
+import styles from "@/styles/Home.module.css";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+
+export const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { isConnected } = useAccount();
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect();
+
+  useEffect(() => {
+    if(!isConnected){
+      setIsLoggedIn(true);
+    }else{
+      setIsLoggedIn(false);
+    }
+  },[isConnected])
+
+  return (
+    <section className={styles.header}>
+      <section className={styles.header_logoSection}>
+        <h1 className={styles.title}>Beans Staking</h1>
+        <Beans fontSize="20px" className={styles.beans}/>
+      </section>
+      <section className={styles.header_btn}>
+        {!isLoggedIn ? (
+          <button className={styles.connectBtn} onClick={disconnect}>
+              DISCONEECT WALLET
+          </button>
+        ) : (
+          <>
+          {connectors.map(connector => (
+            <button disable = {!connector.ready} key={connector.id} onClick={() => connect({connector})} className={styles.connectBtn}>CONNECT WALLET</button>
+          ))}
+          </>
+        ) }
+      </section>
+    </section>
+  )
+}
