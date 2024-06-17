418e265e30d4c248c19dc451a3c22eedfb0fe01a
nilam1995 <72725548+nilam1995@users.noreply.github.com>
2024-06-18 02:57:30 +0530

    files updated

--- frontend/components/main.js
+++ frontend/components/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "@/styles/Home.module.css";
 import { Staking } from './staking';
+import { StakingData } from './stakingData';
 
 export const Main = () => {
   return (
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import { Header } from "@/components/header";
+import { Header } from "../components/header";
+import { Main } from "../components/main";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <title>Beans Staking</title>
     </Head>
     <Header/>
+    <Main/>
    </section>
   );
 }
