918ac4e73bc346eddb7c75be26c454d58fc2f06c
nilam1995 <72725548+nilam1995@users.noreply.github.com>
2024-06-18 18:49:00 +0530

    updated staking.js

--- frontend/components/staking.js
+++ frontend/components/staking.js
@@ -8,7 +8,7 @@ import { StakingABI, StakingAddress } from '../context/constants';
 export const Staking = () => {
   const { isConnected, address } = useAccount();
   const provider = useProvider();
-  const { data:signer } = useSigner();
+  const { data: signer } = useSigner();
 
   const [walletBalance, setWalletBalance] = useState("");
 
@@ -21,18 +21,20 @@ export const Staking = () => {
   const [amount, setAmount] = useState(0);
 
   const toWei = (ether) => ethers.utils.parseEther(ether);
-  const toEther = (wei) => ethers.utils.parseEther(wei);
+  const toEther = (wei) => ethers.utils.formatEther(wei);
 
   useEffect(() => {
     async function getWalletBalance() {
-      await axios.get("http://localhost:5001/getwalletbalance", {
-        params: {address}
-      }).then((response) => {
-        setWalletBalance(response.data.balance);
-      })
+      await axios
+        .get("http://localhost:5001/getwalletbalance", {
+          params: { address },
+        })
+        .then((response) => {
+          setWalletBalance(response.data.balance);
+        });
     }
 
-    if(isConnected){
+    if (isConnected) {
       getWalletBalance();
     }
   }, [isConnected]);
@@ -41,35 +43,36 @@ export const Staking = () => {
     address: StakingAddress,
     abi: StakingABI,
     signerOrProvider: signer || provider,
-  })
+  });
 
   const switchToUnstake = async () => {
-    if(!unstakingTab){
+    if (!unstakingTab) {
       setUnstakingTab(true);
       setStakingTab(false);
       const assetIds = await getAssetIds(address, signer);
       setAssetIds(assetIds);
-      getAssets(assetIds,signer);
+
+      getAssets(assetIds, signer);
     }
-  }
+  };
 
   const switchToStake = () => {
-    if(!stakingTab) {
+    if (!stakingTab) {
       setStakingTab(true);
       setUnstakingTab(false);
     }
-  }
+  };
 
   const getAssetIds = async (address) => {
     const assetIds = await contract.getPositionIdsForAddress(address);
     return assetIds;
-  }
+  };
 
   const calcDaysRemaining = (unlockDate) => {
     const timeNow = Date.now() / 1000;
     const secondsRemaining = unlockDate - timeNow;
     return Math.max((secondsRemaining / 60 / 60 / 24).toFixed(0), 0);
-  }
+  };
 
   const getAssets = async (ids) => {
     const queriedAssets = await Promise.all(
@@ -92,7 +95,7 @@ export const Staking = () => {
 
   const stakeEther = async (stakingLength) => {
     const wei = toWei(String(amount));
-    const data =  { value:wei };
+    const data = { value: wei };
     await contract.stakeEther(stakingLength, data);
   };
 
@@ -104,127 +107,135 @@ export const Staking = () => {
     <section className={styles.stakingContainer}>
       <section>
         <section className={styles.stakeUnstakeTab}>
-          <section className={`${ stakingTab ? styles.stakingType : "" }`}
-          id='stake'
-          onClick={switchToStake}
+          <section
+            className={`${stakingTab ? styles.stakingType : ""}`}
+            id="stake"
+            onClick={switchToStake}
           >
             Stake
           </section>
-          <section className={`${unstakingTab ? styles.stakingType : ""}`}
-          id='unstake'
-          onClick={switchToUnstake}
+          <section
+            className={`${unstakingTab ? styles.stakingType : ""}`}
+            id="unstake"
+            onClick={switchToUnstake}
           >
             Unstake
           </section>
         </section>
         <section className={styles.stakingSection}>
-          <span className={styles.apy}></span>
+          <span className={styles.apy}>7% APY</span>
           {stakingTab ? (
             <section className={styles.stakingBox}>
               <h2>Stake</h2>
               <input
-              className={styles.inputField}
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              type='number'
-              id='inputField'
-              maxLength="120"
-              placeholder='Enter Amount'
-              required
+                className={styles.inputField}
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
               />
               <section className={styles.stakingInfo}>
                 <p>
-                  Balance <span>{(walletBalance / 10 ** 10).toLocaleString()}</span>
+                  Balance:{" "}
+                  <span>{(walletBalance / 10 ** 18).toLocaleString()}</span>
                 </p>
-                <p>Exchange Rate: 1.03582321</p>
+                <p>Exchange Rate: 1.03582967</p>
+                {/* <p>Transaction Cost</p> */}
               </section>
-                <button 
+              <button
                 className={styles.stakeBtn}
                 onClick={() => stakeEther(0, "7%")}
-                >
-                  STAKE
-                </button>
+              >
+                STAKE
+              </button>
             </section>
           ) : (
             <section className={styles.stakingBox}>
               <h2>Unstake</h2>
               <input
-              className={styles.inputField}
-              value={unstakeValue}
-              onChange={(e) => setUnstakeValue(e.target.value)}
-              type='number'
-              id='inputField'
-              maxLength="120"
-              placeholder='Enter Amount'
-              required
+                className={styles.inputField}
+                value={unstakeValue}
+                onChange={(e) => setUnstakeValue(e.target.value)}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
               />
               <section className={styles.stakingInfo}>
                 <p>
-                  Balance:
-                  {assets.length > 0 && assets.map((a,id) => {
-                    if(a.open){
-                      return <span key={id}>{a.etherStaked}</span>
-                    }else{
-                      return <span></span>
-                    }
-                  })}
+                  Balance:{" "}
+                  {assets.length > 0 &&
+                    assets.map((a, id) => {
+                      if (a.open) {
+                        return <span key={id}>{a.etherStaked}</span>;
+                      } else {
+                        return <span></span>;
+                      }
+                    })}
                 </p>
-                <p>You Receive: {unstakeValue == 0 ? "" : unstakeValue * 1.07}</p>
-              </section>  
-              <button 
+                {/* <p>Transaction Cost</p> */}
+                <p>
+                  You Receive: {unstakeValue == 0 ? "" : unstakeValue * 1.07}
+                </p>
+              </section>
+              <button
                 className={styles.stakeBtn}
-                onClick={() => withdraw(assets[assets.length-1].positionId)}
+                onClick={() => withdraw(assets[assets.length - 1].positionId)}
               >
                 UNSTAKE
-              </button>  
+              </button>
             </section>
           )}
         </section>
       </section>
-      <section className={styles.stakingInfoSection}>
-        <section className={styles.stakingInfo}>
-          <h2>Locked Staking</h2>
-          <section className={styles.lockedStaking}>
-            <span>Locked 30 days</span>
-            <span className={styles.lockedStakingAPY}>8% APY</span>
-            <input
-              className={styles.inputField}
-              type='number'
-              id='inputField'
-              maxLength="120"
-              placeholder='Enter Amount'
-              required
-            />
+      <section>
+        <section className={styles.stakingInfoSection}>
+          <section className={styles.stakingInfo}>
+            <h2>Locked Staking</h2>
+            <section className={styles.lockedStaking}>
+              <span>Locked 30 days</span>
+              <span className={styles.lockedStakingAPY}>8% API</span>
+              <input
+                className={styles.inputField}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
+              />
+            </section>
+            <section className={styles.lockedStaking}>
+              <span>Locked 60 days</span>
+              <span className={styles.lockedStakingAPY}>9% API</span>
+              <input
+                className={styles.inputField}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
+              />
+            </section>
+            <section className={styles.lockedStaking}>
+              <span>Locked 90 days</span>
+              <span className={styles.lockedStakingAPY}>12% API</span>
+              <input
+                className={styles.inputField}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
+              />
+            </section>
           </section>
-          <section className={styles.lockedStaking}>
-            <span>Locked 60 days</span>
-            <span className={styles.lockedStakingAPY}>9% APY</span>
-            <input
-              className={styles.inputField}
-              type='number'
-              id='inputField'
-              maxLength="120"
-              placeholder='Enter Amount'
-              required
-            />
-          </section>
-          <section className={styles.lockedStaking}>
-            <span>Locked 90 days</span>
-            <span className={styles.lockedStakingAPY}>12% APY</span>
-            <input
-              className={styles.inputField}
-              type='number'
-              id='inputField'
-              maxLength="120"
-              placeholder='Enter Amount'
-              required
-            />
-          </section>
-          <button className={styles.stakeBtn}>
-            STAKE
-          </button>
+          <button className={styles.stakeBtn}>STAKE</button>
         </section>
       </section>
     </section>
-  )
+  );
 }
--- frontend/context/constants.js
+++ frontend/context/constants.js
@@ -1,291 +1,291 @@
-// import staking from './Staking.json';
+import staking from './Staking.json';
 
 export const StakingAddress = "0x83D55c68705c3B9EBcB2454B1f7999c23701eF52";
-// export const StakingABI = staking.abi;
-export const StakingABI = [
-  {
-    "inputs": [],
-    "stateMutability": "payable",
-    "type": "constructor"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "positionId",
-        "type": "uint256"
-      }
-    ],
-    "name": "closePosition",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "currentPositionId",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "numDays",
-        "type": "uint256"
-      }
-    ],
-    "name": "getInterestRate",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getLockPeriods",
-    "outputs": [
-      {
-        "internalType": "uint256[]",
-        "name": "",
-        "type": "uint256[]"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "positionId",
-        "type": "uint256"
-      }
-    ],
-    "name": "getPositionById",
-    "outputs": [
-      {
-        "components": [
-          {
-            "internalType": "uint256",
-            "name": "positionId",
-            "type": "uint256"
-          },
-          {
-            "internalType": "address",
-            "name": "walletAddress",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "createdDate",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "unlockDatw",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "percentInterest",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "weiStaked",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "weiInterest",
-            "type": "uint256"
-          },
-          {
-            "internalType": "bool",
-            "name": "open",
-            "type": "bool"
-          }
-        ],
-        "internalType": "struct Staking.Position",
-        "name": "",
-        "type": "tuple"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "walletAddress",
-        "type": "address"
-      }
-    ],
-    "name": "getPositionIdsForAddress",
-    "outputs": [
-      {
-        "internalType": "uint256[]",
-        "name": "",
-        "type": "uint256[]"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "lockPeriods",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "owner",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "positionIdsByAddress",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "positions",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "positionId",
-        "type": "uint256"
-      },
-      {
-        "internalType": "address",
-        "name": "walletAddress",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "createdDate",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "unlockDatw",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "percentInterest",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "weiStaked",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "weiInterest",
-        "type": "uint256"
-      },
-      {
-        "internalType": "bool",
-        "name": "open",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "numDays",
-        "type": "uint256"
-      }
-    ],
-    "name": "stakeEther",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "tiers",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  }
-];
+export const StakingABI = staking.abi;
+// export const StakingABI = [
+//   {
+//     "inputs": [],
+//     "stateMutability": "payable",
+//     "type": "constructor"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "positionId",
+//         "type": "uint256"
+//       }
+//     ],
+//     "name": "closePosition",
+//     "outputs": [],
+//     "stateMutability": "nonpayable",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [],
+//     "name": "currentPositionId",
+//     "outputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "numDays",
+//         "type": "uint256"
+//       }
+//     ],
+//     "name": "getInterestRate",
+//     "outputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [],
+//     "name": "getLockPeriods",
+//     "outputs": [
+//       {
+//         "internalType": "uint256[]",
+//         "name": "",
+//         "type": "uint256[]"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "positionId",
+//         "type": "uint256"
+//       }
+//     ],
+//     "name": "getPositionById",
+//     "outputs": [
+//       {
+//         "components": [
+//           {
+//             "internalType": "uint256",
+//             "name": "positionId",
+//             "type": "uint256"
+//           },
+//           {
+//             "internalType": "address",
+//             "name": "walletAddress",
+//             "type": "address"
+//           },
+//           {
+//             "internalType": "uint256",
+//             "name": "createdDate",
+//             "type": "uint256"
+//           },
+//           {
+//             "internalType": "uint256",
+//             "name": "unlockDatw",
+//             "type": "uint256"
+//           },
+//           {
+//             "internalType": "uint256",
+//             "name": "percentInterest",
+//             "type": "uint256"
+//           },
+//           {
+//             "internalType": "uint256",
+//             "name": "weiStaked",
+//             "type": "uint256"
+//           },
+//           {
+//             "internalType": "uint256",
+//             "name": "weiInterest",
+//             "type": "uint256"
+//           },
+//           {
+//             "internalType": "bool",
+//             "name": "open",
+//             "type": "bool"
+//           }
+//         ],
+//         "internalType": "struct Staking.Position",
+//         "name": "",
+//         "type": "tuple"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "address",
+//         "name": "walletAddress",
+//         "type": "address"
+//       }
+//     ],
+//     "name": "getPositionIdsForAddress",
+//     "outputs": [
+//       {
+//         "internalType": "uint256[]",
+//         "name": "",
+//         "type": "uint256[]"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "name": "lockPeriods",
+//     "outputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [],
+//     "name": "owner",
+//     "outputs": [
+//       {
+//         "internalType": "address",
+//         "name": "",
+//         "type": "address"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "address",
+//         "name": "",
+//         "type": "address"
+//       },
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "name": "positionIdsByAddress",
+//     "outputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "name": "positions",
+//     "outputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "positionId",
+//         "type": "uint256"
+//       },
+//       {
+//         "internalType": "address",
+//         "name": "walletAddress",
+//         "type": "address"
+//       },
+//       {
+//         "internalType": "uint256",
+//         "name": "createdDate",
+//         "type": "uint256"
+//       },
+//       {
+//         "internalType": "uint256",
+//         "name": "unlockDatw",
+//         "type": "uint256"
+//       },
+//       {
+//         "internalType": "uint256",
+//         "name": "percentInterest",
+//         "type": "uint256"
+//       },
+//       {
+//         "internalType": "uint256",
+//         "name": "weiStaked",
+//         "type": "uint256"
+//       },
+//       {
+//         "internalType": "uint256",
+//         "name": "weiInterest",
+//         "type": "uint256"
+//       },
+//       {
+//         "internalType": "bool",
+//         "name": "open",
+//         "type": "bool"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "numDays",
+//         "type": "uint256"
+//       }
+//     ],
+//     "name": "stakeEther",
+//     "outputs": [],
+//     "stateMutability": "payable",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "name": "tiers",
+//     "outputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   }
+// ];
